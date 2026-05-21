502c8d50bc9c514344a0a8c67009b12e37426774
UmaMaheswar <uma.i.maheswar@ibm.com>
2026-05-21 09:01:36 +0530

    Latest commits

--- src/main/java/org/example/SpringSecurityDemo.java
+++ src/main/java/org/example/SpringSecurityDemo.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityDemoApplication {
+public class SpringSecurityDemo {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityDemoApplication.class, args);
+        SpringApplication.run(SpringSecurityDemo.class, args);
     }
 
 }
